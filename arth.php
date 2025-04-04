da79f57eff44fd0f79ce92607fba9847a3d514b0
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-04-04 16:08:13 +0530

    arithmetic operator

---
+++ arth.php
@@ -0,0 +1,32 @@
+<?php 
+//arithmetic operators
+// + - * % / **(for power) 
+
+$x=10;
+$y=2;
+$z=null;
+
+//  $z=$x + $y;
+//  $z=$x - $y;
+//  $z=$x * $y;
+//  $z=$x % $y;
+//  $z=$x / $y;
+ $z=$x ** $y;
+
+
+echo $z;
+
+
+
+//increments /decrementts operators
+// ++,--
+
+// Operator precedence
+// ()
+// ** 
+// * / %
+// + -
+
+
+
+?>
