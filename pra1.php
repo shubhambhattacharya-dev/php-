210b036554a3c089f7f655772ba28cf1b385db68
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-04-04 09:49:58 +0530

    variable in php

--- pra1.php
+++ pra1.php
@@ -6,3 +6,16 @@ echo "<h1>Hello world</h1>";
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>
+        order is ready my boy!
+    </button>
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,32 @@
+<?php
+
+
+//variable= a reusable ontainer that holds data 
+// string,integer,float,boolean 
+//php contain space too
+
+
+// for declaring variable in php use $ sign 
+
+$userName="shubham Bhattacharya";
+$food="pizza";
+$age=21;
+
+
+echo($userName);
+
+ //string literal add variable by {} sign  
+
+ echo "<br> hello lord{$userName}<br>";
+ echo "you like {$food}<br>";
+ echo "hii , i am {$userName}, i love {$food} and i am {$age} years old . THANK YOU , HAVE A NICE DAY ";
+
+ //for next line use <br> tag under the "" double ;
+
+ //use string concatnation by using . sign
+ $first="shubham";
+ $last="bhattacharya";
+ echo "{$first}.{$last}";
+
+
+?>
